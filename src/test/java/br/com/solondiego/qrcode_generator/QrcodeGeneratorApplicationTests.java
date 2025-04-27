94b894c0c9afaef6ee8f9af69109054643176848
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2025-04-27 16:31:12 -0300

    Update

---
+++ src/test/java/br/com/solondiego/qrcode_generator/QrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solondiego.qrcode_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
